15b145f2d4e5c81df395cdbc0c547b18381e33e3
Martin Bonnin <martin@mbonnin.net>
2019-07-20 19:57:08 +0200

    automatic url processing

--- src/main/java/net/mbonnin/uploadbin/MainServlet.kt
+++ src/main/java/net/mbonnin/uploadbin/MainServlet.kt
@@ -1,22 +1,40 @@
 package net.mbonnin.uploadbin
 
+import java.util.*
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 
 class MainServlet : HttpServlet() {
+    companion object {
+        val REGEXP = Regex(".*Entity=([^#]*)#.*GOLD_REWARD_STATE.*")
+    }
     override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
         System.out.println("service=${req.servletPath} pathInfo=${req.pathInfo}")
-        
+
         if (req.method != "POST") {
-            resp.sendError(500)
+            resp.sendError(405)
             return
         }
 
-        req.inputStream.use {
-            CloudStorage.bucket.create(req.pathInfo, it)
+        var name:String? = null
+        val lines = req.inputStream.reader().readLines()
+        for (line in lines) {
+            val match = REGEXP.matchEntire(line)
+            if (match != null && match.groupValues[1] != "Mbonnin") {
+                name = match.groupValues[1]
+                break
+            }
         }
+        if (name == null) {
+            resp.sendError(422)
+            return
+        }
+
+        val date = Date()
+        val blobName = String.format("hsdata/${date.year + 1900}_%02d_%02d_$name", date.month + 1, date.date)
+        CloudStorage.bucket.create(blobName, lines.joinToString("\n").byteInputStream())
 
         resp.status = 200
         resp.writer.write("ok")
